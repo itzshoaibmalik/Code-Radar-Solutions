7b7bb2958a5e5c2fdebfaeb739e463c24c97af9c
Shoaib <70048082+itzshoaibmalik@users.noreply.github.com>
2025-02-04 18:53:55 +0530

    Solution to Input and Print in Hexadecimal and Octal

--- Input and Print in Hexadecimal and Octal@@6788d5012db25efec946f941/code.c
+++ Input and Print in Hexadecimal and Octal@@6788d5012db25efec946f941/code.c
@@ -7,10 +7,8 @@ int main() {
     product = num1 * num2;
     intProduct = (int)product;  // Convert float to int for hex & octal
 
-    printf("Product (Decimal): %.2f\n", product);
-    printf("Product (Integer): %d\n", intProduct);
-    printf("Product (Hexadecimal): %x\n", intProduct);
-    printf("Product (Octal): %o\n", intProduct);
+    printf("Hexadecimal: %x\n", intProduct);
+    printf("Octal: %o\n", intProduct);
 
     return 0;
 }
