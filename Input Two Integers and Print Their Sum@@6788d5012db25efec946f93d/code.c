e28e8bf4a1ff64cff0504aaf0a735d7f9e26d795
Shoaib <70048082+itzshoaibmalik@users.noreply.github.com>
2025-02-04 17:27:16 +0530

    Solution to Input Two Integers and Print Their Sum

---
+++ Input Two Integers and Print Their Sum@@6788d5012db25efec946f93d/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, num3, greatest;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    printf("The greatest number is: %d\n", greatest);
+
+    return 0;
+ 
+}
