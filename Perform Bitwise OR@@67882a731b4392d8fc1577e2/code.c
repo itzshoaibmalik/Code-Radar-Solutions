68e85936b61da14f5ce559f1685b34bf737182a4
Shoaib Malik <70048082+itzshoaibmalik@users.noreply.github.com>
2025-02-13 20:43:18 +0530

    Solution to Perform Bitwise OR

--- Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -1,29 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2;
+    int num1, num2, result;
 
     scanf("%d %d", &num1, &num2);
 
-    if (num1 > num2) {
-        printf("num1 is greater than num2\n");
-    } 
-    if (num1 < num2) {
-        printf("num1 is less than num2\n");
-    } 
-    if (num1 == num2) {
-        printf("num1 is equal to num2\n");
-    }
+    result = num1 | num2;
 
-    if (num1 > 0 && num2 > 0) {
-        printf("Both numbers are positive\n");
-    } 
-    if (num1 < 0 || num2 < 0) {
-        printf("At least one number is negative\n");
-    }
-    if (!(num1 > 0)) {
-        printf("num1 is not positive\n");
-    }
+    printf(result)
 
     return 0;
 }
